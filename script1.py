fc1dfa32317cd52faa3fc98a9c0a6637caa16e4f
Rajxkaran <rrajekar@flemingcollege.ca>
2023-05-17 16:15:24 -0400

    Modified both file in the repo clone
    -Deleted a print() statement from script1.py
    -Added one print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 print(f' Hello! Jaremy')
 print(f' How are You?')
-print(f'Hope you are doing well.')
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
 print(f' MY name is Shubham.')
 print(f' I am in the 2nd sem of CSI')
+print(f' Thanks for being my professor.')
